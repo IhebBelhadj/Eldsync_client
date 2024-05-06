4852820cd72051f256f0f33c68ae6fadf7b2b021
Nour <achahlaou.nour@gmail.com>
2024-05-06 01:53:52 +0100

    Added the routing in the menu compoenent of both managements 'chat sync' and 'life Pulse' and adjested the style

--- src/app/front-office/front-office-routing.module.ts
+++ src/app/front-office/front-office-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
     { path: 'lifeline', loadComponent: () => import('./lifeline/lifeline.component').then(m => m.LifelineComponent) },
     { path: 'event', loadChildren: () => import('./event/event.module').then(m => m.EventModule) },
     { path: 'eventUser', loadChildren: () => import('./event-user/event-user.module').then(m => m.EventUserModule) },
-
+    { path: 'chatRoom', loadChildren: () => import('./chat-sync/chat/chat-routing.module').then(m => m.ChatRoutingModule)},
+    { path: 'lifePulse', data: { breadcrumb: 'life Pulse' }, loadChildren:() => import('./lifePulse/lifePulse.module').then((m => m.LifePulseModule))},
 
 ];
 
